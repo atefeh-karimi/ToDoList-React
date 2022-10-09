c416cb9dbdd6047f7555ede7e2ce1b439189b21f
atefeh-karimi <atefehkarimikia0922@gmail.com>
2022-10-09 14:30:17 +0330

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/free-regular-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faBell } from "@fortawesome/free-solid-svg-icons";
 import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
